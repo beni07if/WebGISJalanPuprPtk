95e6723359f4f204daec67a62d907396fe5ee656
beni07if <beni07if@gmail.com>
2022-05-24 20:19:43 +0700

    set halaman index default

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
@@ -23,4 +23,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/index', 'App\Http\Controllers\WebGISController@index')->name('index');
 Route::get('/blog', 'App\Http\Controllers\WebGISController@blog')->name('blog');
-Route::get('/peta', 'App\Http\Controllers\WebGISController@peta')->name('peta');
+Route::get('/', 'App\Http\Controllers\WebGISController@peta')->name('peta');
