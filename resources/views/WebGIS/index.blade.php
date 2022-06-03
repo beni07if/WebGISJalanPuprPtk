e07b441f7b7cddc19d9ad5d2e5028c7040700127
beni07if <beni07if@gmail.com>
2022-06-03 21:35:38 +0700

    update peta2

--- resources/views/WebGIS/index.blade.php
+++ resources/views/WebGIS/index.blade.php
@@ -11,7 +11,7 @@
                 <h2 data-aos="fade-up" data-aos-delay="400">WebGIS PS Ketapang merupakan website yang menyediakan sebaran perhutanan sosial di Kabupaten Ketapang.</h2>
                 <div data-aos="fade-up" data-aos-delay="600">
                     <div class="text-center text-lg-start">
-                        <a href="{{ route('peta') }}" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                        <a href="{{ route('peta2') }}" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                             <span>Jelajah Peta</span>
                             <i class="bi bi-arrow-right"></i>
                         </a>
