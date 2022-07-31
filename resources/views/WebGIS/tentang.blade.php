896110646b8fe5abb8308463350f1cdd5199f5cb
beni07if <beni07if@gmail.com>
2022-07-31 23:41:40 +0700

    Tesstssss

--- resources/views/WebGIS/tentang.blade.php
+++ resources/views/WebGIS/tentang.blade.php
@@ -320,16 +320,16 @@
                     </div> -->
 
                     <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                        <img src="assets/img/foto/infografis/1.png" style="width: 100%;" class="img-fluid" alt="">
+                        <img src="assets/img/foto/infografis/grafis/1.png" style="width: 100%;" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                        <img src="assets/img/foto/infografis/2.png" style="width: 100%;" class="img-fluid" alt="">
+                        <img src="assets/img/foto/infografis/grafis/2.png" style="width: 100%;" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                        <img src="assets/img/foto/infografis/3.png" style="width: 100%;" class="img-fluid" alt="">
+                        <img src="assets/img/foto/infografis/grafis/3.png" style="width: 100%;" class="img-fluid" alt="">
                     </div>
                     <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                        <img src="assets/img/foto/infografis/4.png" style="width: 100%;" class="img-fluid" alt="">
+                        <img src="assets/img/foto/infografis/grafis/4.png" style="width: 100%;" class="img-fluid" alt="">
                     </div>
 
                 </div>
